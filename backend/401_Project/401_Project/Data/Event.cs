8a2021b11479bd81f56a01d6626745a1c51e30bc
Kaitlyn <ksmetler@gmail.com>
2025-03-12 11:51:57 -0600

    Add Database

---
+++ backend/401_Project/401_Project/Data/Event.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _401_Project.Data
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [MaxLength (255)]
+        public string Location { get; set; }
+        [Required]
+        public DateTime EventTime { get; set; }
+        [Required]
+        public DateTime EventCreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
