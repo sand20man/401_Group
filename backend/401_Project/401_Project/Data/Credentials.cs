32e2147cb02800c2f6af8feac4bdc31c552837ec
bybeejac000 <jake.bybee@bybeemail.com>
2025-03-23 16:10:37 -0600

    database connect

---
+++ backend/401_Project/401_Project/Data/Credentials.cs
@@ -0,0 +1,10 @@
+﻿namespace _401_Project.Data
+{
+    public class Credentials
+    {
+
+        public int UserId { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
